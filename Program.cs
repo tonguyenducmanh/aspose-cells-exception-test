f9218c7c9de0d870aca8dc44cb84e4eb3de72aa5
To Nguyen Duc Manh <ducmanh14032000@gmail.com>
2024-06-08 05:19:24 +0700

    init project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Aspose.Cells;
+namespace code{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start project!");
+
+            Workbook wb = new Workbook();
+            Worksheet ws = wb.Worksheets[0];
+            var tempCell = ws.Cells["B1"];
+            tempCell.PutValue("Test cell value");
+            ws.AutoFitRows();
+
+            Console.WriteLine("Stop Project");
+        }
+    }
+
+}
